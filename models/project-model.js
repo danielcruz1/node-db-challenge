c20a94f2010219a3866c5672826c37a645811feb
Daniel Cruz <d.cruz.489@hotmail.com>
2020-02-07 21:28:21 -0700

    router and model complete

---
+++ models/project-model.js
@@ -0,0 +1,31 @@
+const db = require('../data/db-config');
+
+function find() {
+    return db('projects')
+}
+
+function getTask(project_id) {
+    return db('task').where({project_id})
+
+}
+
+function getResource(project_id) {
+    return db('resources').where({project_id})
+}
+
+function addProject(payload) {
+    return db.insert('projects').insert(payload)
+}
+
+
+function addTask(payload) {
+    return db.insert('task').insert(payload)
+}
+
+module.exports ={
+    find,
+    getTask,
+    getResource,
+    addProject,
+    addTask
+} 
