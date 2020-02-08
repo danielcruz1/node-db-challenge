db1878f1f8ddef4d2b5ee1df1146f92e2f5a0ae9
Daniel Cruz <d.cruz.489@hotmail.com>
2020-02-07 20:34:17 -0700

    migrate latest and seed run successful

--- seeds/00-cleanup.js
+++ seeds/00-cleanup.js
@@ -1,6 +1,6 @@
 exports.seed = async function(knex) {
-  await knex('project_tasks').truncate()
-  await knex('project_resources').truncate()
+  await knex('project-tasks').truncate()
+  await knex('project-resources').truncate()
   await knex('task').truncate()
   await knex('resources').truncate()
   await knex('projects').truncate()
